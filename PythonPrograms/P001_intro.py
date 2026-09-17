589149a7d0a2efd7071ffdb4fb7fc37f9c5c2c6a
jay-java <jayprajapati.tops@gmail.com>
2026-09-17 11:08:59 +0530

    datatypes variables

---
+++ PythonPrograms/P001_intro.py
@@ -0,0 +1,17 @@
+#by using print function we can print msg in console(terminal)
+
+print("hello python",end='')
+print('hello python')
+print("hello 12354#%^")
+
+print('hello "automation" testers')
+
+a = 10
+print(a)
+b = 324.453
+print(b)
+
+name ="python"
+print(name)
+
+
---
+++ PythonPrograms/P002_operators.py
@@ -0,0 +1,43 @@
+#arithmetic -> +,-,*,/,%
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+base = 45.45
+height = 345.4
+
+areaOfTriangle = (base*height)/2
+print("area of triangle = ",areaOfTriangle)
+
+
+#assignment -> =,+=,-=,/=,*=
+
+i = 1
+i += 2 # i = i +2
+print(i)
+j = 23
+i += j # i = i+j
+print(i)
+
+i -= 12
+print(i)
+
+i *= 2 # i = i*2
+print(i)
+
+#relational -> >,<,>=,<=,==,!=
+a = 10
+b = 9
+print( a > b)
+print( a < b)
+print( a >= b)
+print( a <= b)
+print( a == b)
+print( a != b)
+
+#logical -> and,or,not
