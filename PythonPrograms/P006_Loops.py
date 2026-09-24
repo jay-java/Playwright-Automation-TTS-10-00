a1e935bad938d6d5f1c3242c08700a36a1cd6d41
jay-java <jayprajapati.tops@gmail.com>
2026-09-24 12:45:46 +0530

    loops pattern

--- PythonPrograms/P006_Loops.py
+++ PythonPrograms/P006_Loops.py
@@ -1,7 +1,7 @@
 # for /for in
 from xml.dom.pulldom import PROCESSING_INSTRUCTION
 
-for i in range(1, 11):
+for i in range(5, 11):
     print(i)
 
 for a in range(10):
@@ -30,3 +30,29 @@ for i in range(1,6): #row
         print("*",end='')
     print()
 
+
+for i in range(10,0,-1):
+    print(i)
+
+
+for i in range(1,6): #row
+    for s in range(4,i-1,-1):
+        print(" ",end='')
+    for j in range(1,i+1): #column
+        print("*",end='')
+    print()
+
+for i in range(1,6): #row
+    for s in range(4,i-1,-1):
+        print(" ",end='')
+    for j in range(1,i+1): #column
+        print("* ",end='')
+    print()
+
+
+for i in range(1,6):
+    for s in range(2,i+1):
+        print(" ",end='')
+    for j in range(5,i-1,-1):
+        print("* ",end='')
+    print()
